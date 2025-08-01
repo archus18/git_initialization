c151652946a030582354ec34e07fa78cd392e583
Archana_Sonawane <161570465+archus18@users.noreply.github.com>
2025-08-01 18:21:47 +0530

    Update first.java

--- first.java
+++ first.java
@@ -3,6 +3,7 @@ public class first
 public static void main(String[] args) {
     System.out.println("Hello From first class");
     System.out.println ("Hello from upgrad training center");
+     System.out.println ("this is for pull command");
 }
 
 }
