e236ac798944992215efc24f03c708bd9ad40ed5
archus18 <archanasonawane4534@gmail.com>
2025-08-01 18:00:04 +0530

    first commit on newbranch

--- first.java
+++ first.java
@@ -2,6 +2,7 @@ public class first
 {
 public static void main(String[] args) {
     System.out.println("Hello From first class");
+    System.out.println ("Hello from upgrad training center");
 }
 
 }
