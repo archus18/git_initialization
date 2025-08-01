dafce9aa986ff59a7ea21d8eb748ecf7af2cf3f6
archus18 <archanasonawane4534@gmail.com>
2025-08-01 18:40:25 +0530

    collaboration

--- first.java
+++ first.java
@@ -3,7 +3,10 @@ public class first
 public static void main(String[] args) {
     System.out.println("Hello From first class");
     System.out.println ("Hello from upgrad training center");
-     System.out.println ("this is for pull command");
+    System.out.println ("this is for pull command");
+    System.out.println ("this is for collabration");
+
+
 }
 
 }
